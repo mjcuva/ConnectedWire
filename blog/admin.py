f37776d65d969d9bffdc3975a9dc6001bbd3a992
Marc <mjcuva@gmail.com>
2012-08-24 16:42:00 -0500

    Ignore

--- blog/admin.py
+++
@@ -1,7 +0,0 @@
-from blog.models import Post, User, Categories, Page
-from django.contrib import admin
-
-admin.site.register(Post)
-admin.site.register(User)
-admin.site.register(Categories)
-admin.site.register(Page)

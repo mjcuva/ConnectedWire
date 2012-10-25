07ea359341e477ca852ddaf1a73482e219c2f6b0
Marc <mjcuva@gmail.com>
2012-10-25 10:07:17 -0500

    Removed mobile in favor of responsive design

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-#Allows the page to be rendered
+# Allows the page to be rendered
 from django.shortcuts import render_to_response
 
 # Allows redirects, as well as 404 pages
@@ -49,51 +49,34 @@ import util
 #Form templates
 import blogForms
 
-#PIL Image Library
+# PIL Image Library
 from PIL import Image
 
-#Time Function
+# Time Function
 import datetime
 
+# Allows posting to Twitter
 import twitter
 
-import string
-
-import re
-
+# Stats for the dashboard
 import stats
 
+# OS Paths for files
 import paths
 
+# Check if in debug mode
 from django.conf import settings
-
-# Mobile user agents
-reg_b = re.compile(r"android.+mobile|avantgo|bada\\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|meego.+mobile|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino", re.I|re.M)
-reg_v = re.compile(r"1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\\-(n|u)|c55\\/|capi|ccwa|cdm\\-|cell|chtm|cldc|cmd\\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\\-s|devi|dica|dmob|do(c|p)o|ds(12|\\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\\-|_)|g1 u|g560|gene|gf\\-5|g\\-mo|go(\\.w|od)|gr(ad|un)|haie|hcit|hd\\-(m|p|t)|hei\\-|hi(pt|ta)|hp( i|ip)|hs\\-c|ht(c(\\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\\-(20|go|ma)|i230|iac( |\\-|\\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\\/)|klon|kpt |kwc\\-|kyo(c|k)|le(no|xi)|lg( g|\\/(k|l|u)|50|54|\\-[a-w])|libw|lynx|m1\\-w|m3ga|m50\\/|ma(te|ui|xo)|mc(01|21|ca)|m\\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\\-2|po(ck|rt|se)|prox|psio|pt\\-g|qa\\-a|qc(07|12|21|32|60|\\-[2-7]|i\\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\\-|oo|p\\-)|sdk\\/|se(c(\\-|0|1)|47|mc|nd|ri)|sgh\\-|shar|sie(\\-|m)|sk\\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\\-|v\\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\\-|tdg\\-|tel(i|m)|tim\\-|t\\-mo|to(pl|sh)|ts(70|m\\-|m3|m5)|tx\\-9|up(\\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\\-|your|zeto|zte\\-", re.I|re.M)
-
-# Checks if user is mobile
-def checkMobile(request):
-    request.mobile = False
-    # If there is a user agent
-    if request.META.has_key('HTTP_USER_AGENT'):
-        #Checks the user agent agianst list of agents
-        user_agent = request.META['HTTP_USER_AGENT']
-        b = reg_b.search(user_agent)
-        v = reg_v.search(user_agent[0:4])
-        if b or v:
-            return False #IMPORTANT -- CHANGED FOR STYLESHEET TESTING
-        else:
-            return False
-
-
-
-
 
 
 # Homepage
 def index(request):
-    
-    isMobile = checkMobile(request)
+
+    # Check if user is on IE 8
+    ie = False
+    if request.META.has_key('HTTP_USER_AGENT'):
+        user_agent = request.META['HTTP_USER_AGENT']
+        if user_agent.find('MSIE 8.0') != -1:
+            ie = True
     
     # Checks if a user is logged in
     username = util.checkLoggedIn(request)
@@ -103,9 +86,7 @@ def index(request):
     
     # Gets the templates
     template = "base.html"
-    mobileTemplate = "mobile/base.html"
     page_template = "index.html"
-    mobilePage_template = "mobile/index.html"
     
     # If the request is coming from an ajax request
     # and sets the template accordingly
@@ -152,10 +133,12 @@ def index(request):
     
     # Renders the page
     
-    if not isMobile:
+    # If on IE, show message asking user to use another broswer
+    if not ie:
         return render_to_response(template, {"posts" : posts, "pages":pages, "username": username, "wordcount": count, "featured": featured, "cats":cats}, context_instance=RequestContext(request))
     else:
-        return render_to_response(mobileTemplate, {"posts": posts}, context_instance=RequestContext(request))
+        return render_to_response('ie.html')
+
 
 # Loads the archive for a specific year
 def archive(request, year):
@@ -186,15 +169,12 @@ def archive(request, year):
 #Loads the page for a specific category
 def category(request, category):
     
-    isMobile = checkMobile(request)
 
     
     username = util.checkLoggedIn(request)
     
     template = "base.html"
-    mobileTemplate = "mobile/base.html"
     page_template = "index.html"
-    mobilePage_template = "mobile/index.html"
     
     if request.is_ajax():
         template = page_template
@@ -210,10 +190,9 @@ def category(request, category):
     
 
     
-    if not isMobile:
-        return render_to_response(template, {"posts" : posts, "pages":pages, "username": username}, context_instance=RequestContext(request))
-    else:
-        return render_to_response(mobileTemplate, {"posts": posts}, context_instance=RequestContext(request))
+    
+    return render_to_response(template, {"posts" : posts, "pages":pages, "username": username}, context_instance=RequestContext(request))
+
 
 #Creates a new user
 def signup(request):
